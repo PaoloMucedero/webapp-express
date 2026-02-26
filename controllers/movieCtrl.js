8f8b245b0ff8d5dad1463ba58a0ce7fc8eb5f7bb
PaoloMucedero <p.mucedero@gmail.com>
2026-02-26 18:57:31 +0100

    Importata rotta index

--- controllers/movieCtrl.js
+++ controllers/movieCtrl.js
@@ -5,15 +5,32 @@ const connection = require(`../data/db`);
 
 // INDEX
 function index(req, res) {
-    console.log(`sei sulla rotta index`);
     // TEMPORARY DEBUG
-    res.send("sei sulla rotta index")
+    //console.log(`sei sulla rotta index`);
+    //res.send("sei sulla rotta index")
+    // PREPARO LA QUERY
+    const sql = "SELECT * FROM movies"
+    // ESEGUO LA QUERY
+    connection.query(sql, (err, results) => {
+
+        if (err) {
+            console.error("Errore database:", err);
+            return res.status(500).json({
+                error: "Errore del server"
+            });
+        }
+
+        res.json(results);
+
+    });
+
 }
+
 
 // SHOW
 function show(req, res) {
-    console.log(`sei sulla rotta show`);
     // TEMPORARY DEBUG
+    console.log(`sei sulla rotta show`);
     res.send("sei sulla rotta show")
 }
 
